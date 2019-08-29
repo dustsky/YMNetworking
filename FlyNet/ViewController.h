a2e225017c27589136ff3ee7feb56259957288bd
dustsky <gsmxsky@gmail.com>
2019-08-30 00:29:09 +0800

    init

---
+++ FlyNet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlyNet
+//
+//  Created by DUSTSKY on 2019/8/30.
+//  Copyright © 2019 DUSTSKY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FlyNet/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  FlyNet
+//
+//  Created by DUSTSKY on 2019/8/30.
+//  Copyright © 2019 DUSTSKY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
